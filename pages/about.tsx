c3568fe97c63d28380da4de2f6eea3c599d6bea0
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-15 10:30:39 -0300

    progress class 162 section 11

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/layouts"
+
+const AboutPage = () => {
+    return (
+        <Layout>
+            <h1>About Page</h1>
+        </Layout>
+    );
+}
+
+export default AboutPage;
---
+++ pages/theme-changer.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/layouts"
+
+const ThemeChangerPage = () => {
+    return (
+        <Layout>
+            <h1>Theme Changer Page</h1>
+        </Layout>
+    );
+}
+
+export default ThemeChangerPage;
